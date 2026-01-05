cd68bd320afb45f77b04c562532b43e9532829d1
aabhaysharmaa <abhaysharma9667200@gmail.com>
2026-01-05 18:50:04 +0530

    feat : loading state for chat setting page

--- src/app/(browse)/_components/sidebar/title-skeleton.tsx
+++ src/app/(browse)/_components/sidebar/title-skeleton.tsx
@@ -1,10 +1,11 @@
 import { Skeleton } from "@/components/ui/skeleton"
+import { cn } from "@/lib/utils"
 
 
-export const TitleSkeleton = () => {
+export const TitleSkeleton = ({ className }: { className?: string }) => {
 	return (
 		<div className="px-4 my-3">
-			<Skeleton className="w-30 px-4 py-4" />
+			<Skeleton className={cn("w-30 px-4 py-4",className)} />
 		</div>
 	)
 }

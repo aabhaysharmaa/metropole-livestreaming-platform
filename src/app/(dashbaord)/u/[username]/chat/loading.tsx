ef63eb964289e167754db92e16a9ef6cac8d2fd8
aabhaysharmaa <abhaysharma9667200@gmail.com>
2026-01-05 18:53:40 +0530

    fix : resize the titleSkeleton for chat setting

--- src/app/(dashbaord)/u/[username]/chat/loading.tsx
+++ src/app/(dashbaord)/u/[username]/chat/loading.tsx
@@ -5,7 +5,7 @@ import { TitleSkeleton } from '@/app/(browse)/_components/sidebar/title-skeleton
 const Loading = () => {
 	return (
 		<div className='space-y-6 p-4'>
-			<TitleSkeleton className='p-4 h-11 w-38 mb-4' />
+			<TitleSkeleton className='p-4 h-12 w-50 mb-4' />
 			<ToggleCardSkeleton />
 			<ToggleCardSkeleton />
 			<ToggleCardSkeleton />
